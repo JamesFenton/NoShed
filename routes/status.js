b7c758b4d4747f3e9773343296d3ede0643c9738
James Fenton <jamesfenton777@gmail.com>
2015-04-27 15:58:42 +0200

    Made eskom.co.za calls happen once every 15 seconds instead of on every request

--- routes/status.js
+++ routes/status.js
@@ -3,53 +3,71 @@ var http = require('http');
 var router = express.Router();
 var log = require('../helpers/logging');
 
+var timeOfLastUpdate = null;
+var currentStatus = null;
+
 // route for /status
 router.get('/', function (req, res) { 
 	
-		// headers
-		res.setHeader('Access-Control-Allow-Origin', '*'); // Website you wish to allow to connect
-		//res.setHeader('Access-Control-Allow-Methods', 'GET'); // Request methods you wish to allow
-		//res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // Request headers you wish to allow
-		//res.setHeader('Access-Control-Allow-Credentials', true); // Set to true if you need the website to include cookies in the requests sent
-		
-		res.setHeader('Content-Type', 'application/json');
-		
+	// headers
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Website you wish to allow to connect
+	//res.setHeader('Access-Control-Allow-Methods', 'GET'); // Request methods you wish to allow
+	//res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // Request headers you wish to allow
+	//res.setHeader('Access-Control-Allow-Credentials', true); // Set to true if you need the website to include cookies in the requests sent
+
+	res.setHeader('Content-Type', 'application/json');
+
+	var sendResponse = function(req, res, status) {
+		console.log('\tSending status: ' + status);
+		res.send({'status': status});
+		// logging
+		var clientAddress = req.header('x-forwarded-for') || req.connection.remoteAddress || 'unknown';
+		log(clientAddress, 'debug', status, null);
+	};
+
+	var timeSinceLastUpdate = new Date() - timeOfLastUpdate;
+	console.log("Last update was " + timeSinceLastUpdate/1000 + " seconds ago");
+
+	// update status every 15 seconds
+	var timeBetweenStatusUpdates = 15000; // milliseconds
+	if(timeOfLastUpdate === null || currentStatus === null || timeSinceLastUpdate > timeBetweenStatusUpdates)
+	{
 		// call eskom.co.za
 		var options = {
-		  host: 'loadshedding.eskom.co.za',
-		  port: 80,
-		  path: '/LoadShedding/GetStatus'
+			host: 'loadshedding.eskom.co.za',
+			port: 80,
+			path: '/LoadShedding/GetStatus'
 		};
-        
 		http.get(options, function(getRes) {
-            console.log("\tGot response from eskom.co.za: " + getRes.statusCode);
-            if(getRes.statusCode === 200) {
-                // send response
-                getRes.on('data', function (chunk) {
-                    console.log('\tBODY: ' + chunk);
-                    var status = parseInt(chunk) - 1;
-                    console.log('\tSending status: ' + status);
-                    res.send({'status': status});
-                    // logging
-                    var clientAddress = req.header('x-forwarded-for') || req.connection.remoteAddress || 'unknown';
-                    log(clientAddress, 'debug', status, null);
-                });
-            }
-            else {
-                var message = 'Could not connect to eskom.co.za: ' + getRes.statusCode + '. ' + getRes.statusMessage;
-                res.status(500).send({'error': message});
-                var clientAddress = req.header('x-forwarded-for') || req.connection.remoteAddress || 'unknown';
-                log(clientAddress, 'error', null, message);
-            }
-			
+			timeOfLastUpdate = new Date();
+			console.log("\tGot response from eskom.co.za: " + getRes.statusCode);
+			// if a successful response from eskom.co.za
+			if(getRes.statusCode === 200) {
+				// send response
+				getRes.on('data', function (chunk) {
+						console.log('\tBODY: ' + chunk);
+						var status = parseInt(chunk) - 1;
+						currentStatus = status;
+						sendResponse(req, res, status);
+				});
+			}
+			else {
+				var message = 'Could not connect to eskom.co.za: ' + getRes.statusCode + '. ' + getRes.statusMessage;
+				res.status(500).send({'error': message});
+				var clientAddress = req.header('x-forwarded-for') || req.connection.remoteAddress || 'unknown';
+				log(clientAddress, 'error', null, message);
+			}
 		// handle errors
 		}).on('error', function(e) {
-            var message = 'Could not connect to eskom.co.za: ' + e.message;
+			var message = 'Could not connect to eskom.co.za: ' + e.message;
 			res.status(500).send({'error': message});
-            var clientAddress = req.header('x-forwarded-for') || req.connection.remoteAddress || 'unknown';
-            log(clientAddress, 'error', null, message);
+			var clientAddress = req.header('x-forwarded-for') || req.connection.remoteAddress || 'unknown';
+			log(clientAddress, 'error', null, message);
 		});
 	}
-);
+	else {
+		sendResponse(req, res, currentStatus);
+	}
+});
 
 module.exports = router;
