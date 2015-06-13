e7255e577d4489c2adc486d3296cbf8d4ed651e5
James Fenton <jamesfenton777@gmail.com>
2015-06-13 20:33:13 +0200

    Updated seed.js and area.seeder.js to include saving of areas

--- area_data/area.seeder.js
+++ area_data/area.seeder.js
@@ -1,35 +1,58 @@
 var mongoose = require('../config/mongoose');
 mongoose();
-var AreaInfo = require('mongoose').model('AreaInfo');
 
-var readData = function(filePath){
+// Areas
+var Area = require('mongoose').model('Area');
+
+var readAreas = function(filePath){
+	// read areas from file and return array of Area
+};
+
+var saveAreasFromFile = function(filePath) {
+	var areaData = readAreas(filePath);
+	// save data to db
+	for(var i = 0; i < areaData.length; i++){
+		var area = areaData[i];
+		console.log("Saving: " + JSON.stringify(area));
+		area.save(function (err, logEntry) {
+			if (err) { 
+				return console.error(err);
+			}
+		});
+	}
+};
+
+// Area schedules
+var AreaSchedule = require('mongoose').model('AreaSchedule');
+
+var readAreaSchedule = function(filePath){
 	// read data from csv and return array of AreaInfo
-}
+};
 
-var saveAreaData = function(data){
-	// save the data
+var saveAreaSchedule = function(data){
+	// save data to db
 	for(var i = 0; i < data.length; i++){
-		logEntry = data[i];
-		console.log("saving: " + JSON.stringify(logEntry));
+		var logEntry = data[i];
+		console.log("Saving: " + JSON.stringify(logEntry));
 		logEntry.save(function (err, logEntry) {
 			if (err) { 
 				return console.error(err);
 			}
 		});
 	}
-}
+};
 
-var seedFromFiles = function(csvFileNames){
+var saveAreaSchedulesFromFile = function(csvFileNames){
 	for(var fileIndex = 0; fileIndex < csvFileNames.length; csvFileNames++) {
 		var fileName = csvFileNames[fileIndex];
 		// read file data
-		var data = readData(fileName);
+		var data = readAreaSchedule(fileName);
 		// save the data
-		saveAreaData(data);
+		saveAreaSchedule(data);
 	}
-}
+};
 
 module.exports = {
-	saveAreaData: saveAreaData,
-	seedFromFile: seedFromFiles
+	saveAreas: saveAreasFromFile,
+	saveAreaSchedulesFromFile: saveAreaSchedulesFromFile
 };
